6790c033bb3b76604b2e744cd0211f73936629a0
WisleyASousa <wisleya.sousa@outlook.com.br>
2022-09-06 11:26:42 -0300

    finalizando biblioteca

--- http-validacao.js
+++ http-validacao.js
@@ -1,23 +1,42 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
+
+function manejaErros(erro) {
+    throw new Error(erro.message);
+}
+
+
 async function checaStatus(arrayURLs) {
     //promises async await
-    const arrayStatus = await Promise.all(arrayURLs.map(async url => {
-        const res = await fetch(url)
-        return res.status;
+    try{const arrayStatus = await Promise
+        .all(arrayURLs
+            .map(async url => {
+                const res = await fetch(url)
+                return `${res.status} - ${res.statusText}`;
     }))
     return arrayStatus;
+    }catch(erro){
+        manejaErros(erro);
+    }
+    
 }
 
 
 
 function geraArrayDeURLs(arrayLinks) {
     //loop para cada { chave: valor }
-    return arrayLinks.map(ObjectLink => Object.values(ObjectLink).join());
+    return arrayLinks
+        .map(ObjectLink => Object
+            .values(ObjectLink).join());
 }
 
 export async function validaURLs(arrayLinks) {
     const links = geraArrayDeURLs(arrayLinks); 
     const statusLinks = await checaStatus(links);
-    return statusLinks;
+    //spread operador(espalhamento)
+    const resultados = arrayLinks.map((objeto, indice) => ({
+        ...objeto,
+        status: statusLinks[indice]
+    }))
+    return resultados;
 }
